871d989e2c35ab29bab8e6cf72307dad053e1e04
Antonio Fernández Marín <antonio.fernandez@Aytos.local>
2023-04-28 14:46:19 +0200

    commit con insert de citas, modificar casi, y get citas

--- index.js
+++ index.js
@@ -90,7 +90,34 @@ app.post('/cliente', async (req, res) => {
 
 
 
-//Insertar admin 
+//Insertar citas 
+app.post('/citas', async (req, res) => {
+  const {
+    fecha_cita,
+    nombre_cliente,
+    nombre_gym,
+    hora_cita
+  } = req.body;
+  pool.query(
+    'INSERT INTO cita (fecha_cita, id_cliente, id_gym,hora_cita) VALUES ($1, (SELECT id_cliente FROM cliente WHERE nombre_cliente = $2), (SELECT id_gym FROM gimnasio WHERE nombre_gym = $3),$4);',
+    [
+      fecha_cita,
+      nombre_cliente,
+      nombre_gym,
+      hora_cita
+    ],
+    (err, result) => {
+      if (err) {
+        console.error('Error al crear usuario', err);
+        res.status(500).send(JSON.stringify({ message: 'Error al crear cita' }));
+      } else {
+        res.status(201).send(JSON.stringify({ message: 'Cita creado exitosamente' }));
+      }
+    }
+  );
+});
+
+
 app.post('/admin', async (req, res) => {
   const {
     nombre_responsable,
@@ -133,8 +160,6 @@ app.post('/admin', async (req, res) => {
     }
   );
 });
-
-
 
 
 
@@ -216,6 +241,48 @@ app.get('/gimnasios', async (req, res) => {
   } catch (error) {
     console.error(error);
     res.sendStatus(500); // Devuelve un código de estado 500 si hay algún error
+  }
+});
+
+
+
+// Endpoint para obtener la lista de citas
+app.get('/get-citas/:id_cliente', async (req, res) => {
+  try {
+    const id_cliente = req.params.id_cliente;
+    const query = `SELECT ci.id_cita, TO_CHAR(ci.fecha_cita, 'DD-MM-YYYY'), ci.hora_cita, g.nombre_gym FROM cita ci JOIN cliente c ON c.id_cliente = c.id_cliente JOIN gimnasio g ON g.id_gym=ci.id_gym WHERE c.id_cliente =$1 `; 
+    const result = await pool.query(query, [id_cliente]);
+     // eslint-disable-next-line no-console
+    /*  console.log(result) */
+    const cita = result.rows.map(rows => rows); // Obtiene solo los nombres de los gimnasios
+
+    res.send(cita); 
+    // eslint-disable-next-line no-console
+    console.log(cita)
+     // eslint-disable-next-line no-console
+     /* console.log(id_cliente) */
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500); // Devuelve un código de estado 500 si hay algún error
+  }
+});
+
+//Editar cita 
+app.put('/citas/:id_cita', async (req, res) => {
+  try {
+    const id = req.params.id_cita;
+    const { fecha_cita, hora_cita } = req.body;
+
+    const { rows } = await pool.query('UPDATE cita SET fecha_cita = $1, hora_cita = $2 WHERE id = $3 RETURNING *', [fecha_cita, hora_cita, id]);
+    
+    if (!rows[0]) {
+      return res.status(404).json({ mensaje: 'Cita no encontrada' });
+    }
+
+    res.status(200).json(rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: 'Error interno del servidor' });
   }
 });
 
