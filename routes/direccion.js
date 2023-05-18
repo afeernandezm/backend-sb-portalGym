33491114a141d797807b0af9bf15944a803f044b
Antonio Fernández Marín <antonio.fernandez@Aytos.local>
2023-05-18 13:13:23 +0200

    tocado el package.json sección engines

--- routes/direccion.js
+++ routes/direccion.js
@@ -5,20 +5,6 @@ const router = express.Router();
 const apiKey = "WuT5AJaMhcIbVEMx02J5";
 const apiUrl = "https://api.maptiler.com/geocoding/";
 
-/* router.get("/direcciones/geocode", async (req, res) => {
-  const { address } = req.query;
-
-  try {
-    const response = await axios.get(
-      `${apiUrl}${encodeURIComponent(address)}.json?key=${apiKey}`
-    );
-
-    res.json(response.data);
-  } catch (error) {
-    console.error("Error en la solicitud de geocodificación:", error);
-    res.status(500).json({ error: "Error en la geocodificación" });
-  }
-}); */
 router.get("/direcciones/geocode", async (req, res) => {
   const { address } = req.query;
 
